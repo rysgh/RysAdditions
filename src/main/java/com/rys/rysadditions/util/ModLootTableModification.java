1a897fa57825b42760190a14d38c1fed4055d1cd
ry <98239048+rysgh@users.noreply.github.com>
2023-06-19 08:32:20 +0100

    Fixed random crate selection when Fishing and renamed Group

--- src/main/java/com/rys/rysadditions/util/ModLootTableModification.java
+++ src/main/java/com/rys/rysadditions/util/ModLootTableModification.java
@@ -1,15 +1,14 @@
 package com.rys.rysadditions.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.block.Block;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.GroupEntry;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static com.rys.rysadditions.block.ModBlocks.CRATE;
 import static com.rys.rysadditions.block.ModBlocks.DEEP_SEA_CRATE;
@@ -25,20 +24,11 @@ public class ModLootTableModification {
             if(FISHING_TREASURE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                     .rolls(ConstantLootNumberProvider.create(1))
-                    .conditionally(RandomChanceLootCondition.builder(0.080f))
-                    .with(ItemEntry.builder(randCrate()))
+                    .conditionally(RandomChanceLootCondition.builder(1.00f))
+                    .with(GroupEntry.create(ItemEntry.builder(CRATE), ItemEntry.builder(DEEP_SEA_CRATE), ItemEntry.builder(JEWEL_CRATE), ItemEntry.builder(PLANT_CRATE), ItemEntry.builder(RATION_CRATE)).build())
                     .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
         });
-    }
-
-    private static Block randCrate() {
-        Block selectedCrate;
-        Block[] crates =  {CRATE, DEEP_SEA_CRATE, JEWEL_CRATE, PLANT_CRATE, RATION_CRATE};
-
-        selectedCrate = crates[ThreadLocalRandom.current().nextInt(0, crates.length + 1)];
-
-        return selectedCrate;
     }
 }
